97133f8b6966203ecfbd791b6978b8dbd44e6d18
Ivan Añora <anorajivan@gmail.com>
2022-09-06 20:15:30 +0800

    Add link to repository

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { BsGithub } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <div className="flex h-16 mx-auto px-4 bg-green-600 text-white items-center justify-center">
+      <a href="https://github.com/ivananora/shopping-cart" className="mr-4">
+        <BsGithub size="28" />
+      </a>
       <p className="mr-4">
         Sign up for updates on promotions, new products, and sales.
       </p>
