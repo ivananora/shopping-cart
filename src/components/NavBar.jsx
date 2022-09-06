e69154b24e0607783012c010587646daa20da6f2
Ivan Añora <anorajivan@gmail.com>
2022-09-06 17:22:15 +0800

    Update navbar style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,14 +3,14 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 
 const NavBar = () => {
   return (
-    <div className="flex justify-between items-center h-20 max-w-[1240px] mx-auto">
+    <div className="flex justify-between items-center sticky top-0 z-50 h-20 max-w-[1240px] mx-auto bg-white/50 backdrop-blur-sm">
       <h1 className="w-full text-3xl font-bold italic text-green-600 uppercase">
         minimal1st
       </h1>
       <ul className="flex">
         <li className="p-4">Store</li>
         <li className="p-4">About</li>
-        <li className="p">
+        <li className="py-4 pl-4">
           <AiOutlineShoppingCart size="20" />
         </li>
       </ul>
