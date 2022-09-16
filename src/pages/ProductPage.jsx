b75b56b188784e9b26260a15bea75a4c1275df77
Ivan Añora <anorajivan@gmail.com>
2022-09-16 17:19:11 +0800

    Update product page

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -10,7 +10,7 @@ import {
 
 const ProductPage = () => {
   const product = useSelector((state) => state.product);
-  const { image, title, price, description } = product;
+  const { image, title, price, description, category, rate } = product;
   const { productId } = useParams();
   const dispatch = useDispatch();
   console.log(product);
@@ -38,23 +38,29 @@ const ProductPage = () => {
   };
 
   return (
-    <div className="flex flex-col h-[36rem] w-96 min-w-[295px] min-h-[332px] p-5 justify-between bg-white border-2 rounded drop-shadow-sm border-gray-100">
+    <div className="grid gap-5 mx-4 my-10 p-5 max-w-[1280px] grid-cols-1 md:grid-cols-2 border-2 rounded drop-shadow-sm border-gray-100">
       {Object.keys(product).length === 0 ? (
         <div>...Loading</div>
       ) : (
         <>
-          <div className="flex items-center justify-center max-h-[208px]">
+          <div className="flex items-center justify-center max-h-[500px]">
             <img className="w-auto max-h-full" src={image} alt={title} />
           </div>
-          <div className="h-96">
-            <h5 className="mb-2 h-7 truncate text-lg tracking-tight text-gray-800">
-              {title}
-            </h5>
-            <p className="mb-2 text-gray-700 text-xl font-bold">
+          <div className="flex flex-col h-full">
+            <h5 className="text-3xl tracking-tight">{title}</h5>
+            <p>{rate}</p>
+            <p className="text-gray-400 mb-1">{category}</p>
+            <p className="mb-2 text-3xl font-bold text-green-700">
               ₱{roundUpNearestTen(Math.floor(price * 57))}
             </p>
-            <p className="text-gray-500 text-base">{description}</p>
-            <button onClick={() => dispatch(addToCart(product))}>
+            <div className="mb-2">
+              <p className="font-bold">Description</p>
+              <p>{description}</p>
+            </div>
+            <button
+              className="rounded bg-green-700 text-white py-3 px-6 m-1 hover:bg-green-600 sm:w-40"
+              onClick={() => dispatch(addToCart(product))}
+            >
               Add to cart
             </button>
           </div>
